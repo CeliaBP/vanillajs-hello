3acf26eb59c1066c470474b1d83c6c9c0597966c
CeliaBP <Celia.bcn28@gmail.com>
2024-09-13 11:54:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+//write your code here
+function generateExcuse() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broke"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoRan = Math.floor(Math.random() * who.length);
+  let whatRan = Math.floor(Math.random() * what.length);
+  let whenRan = Math.floor(Math.random() * when.length);
+
+  return who[whoRan] + " " + what[whatRan] + " " + when[whenRan] + ".";
+}
